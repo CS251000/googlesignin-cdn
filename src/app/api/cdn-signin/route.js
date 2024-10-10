5bcccf8847314f989da432f33459ccd120414c2c
chirag <chiragsinghal04@gmail.com>
2024-10-10 19:45:11 +0530

    -f

--- src/app/api/cdn-signin/route.js
+++ src/app/api/cdn-signin/route.js
@@ -1,42 +1,42 @@
-(function() {
-    function loadGoogleSignInScript() {
-      const script = document.createElement("script");
-      script.src = "https://accounts.google.com/gsi/client";
-      script.async = true;
-      script.onload = initializeGoogleSignIn;
-      document.body.appendChild(script);
-    }
+// (function() {
+//     function loadGoogleSignInScript() {
+//       const script = document.createElement("script");
+//       script.src = "https://accounts.google.com/gsi/client";
+//       script.async = true;
+//       script.onload = initializeGoogleSignIn;
+//       document.body.appendChild(script);
+//     }
 
-    function handleCredentialResponse(response) {
-      const jwtToken = response.credential;
-      console.log("Encoded JWT ID token: " + jwtToken);
+//     function handleCredentialResponse(response) {
+//       const jwtToken = response.credential;
+//       console.log("Encoded JWT ID token: " + jwtToken);
       
-    }
+//     }
   
-    function initializeGoogleSignIn() {
-      window.google.accounts.id.initialize({
-        client_id: "770883517770-b6ffrsubm5trevp14cac76qnk533igvl.apps.googleusercontent.com",
-        callback: handleCredentialResponse,
-        auto_select: true,
-        context: "signin",
-        ux_mode: "popup",
-        itp_support: true
-      });
-      window.google.accounts.id.prompt();
+//     function initializeGoogleSignIn() {
+//       window.google.accounts.id.initialize({
+//         client_id: "770883517770-b6ffrsubm5trevp14cac76qnk533igvl.apps.googleusercontent.com",
+//         callback: handleCredentialResponse,
+//         auto_select: true,
+//         context: "signin",
+//         ux_mode: "popup",
+//         itp_support: true
+//       });
+//       window.google.accounts.id.prompt();
 
-      window.google.accounts.id.renderButton(
-        document.getElementById("g_id_signin"), {
-          type: "standard",
-          shape: "pill",
-          theme: "filled_blue",
-          text: "continue_with",
-          size: "large",
-          logo_alignment: "left"
-        }
-      );
-    }
-    window.onload = function() {
-      loadGoogleSignInScript();
-    };
-  })();
+//       window.google.accounts.id.renderButton(
+//         document.getElementById("g_id_signin"), {
+//           type: "standard",
+//           shape: "pill",
+//           theme: "filled_blue",
+//           text: "continue_with",
+//           size: "large",
+//           logo_alignment: "left"
+//         }
+//       );
+//     }
+//     window.onload = function() {
+//       loadGoogleSignInScript();
+//     };
+//   })();
   
